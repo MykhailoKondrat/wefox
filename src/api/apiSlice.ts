a1074e33da393e3d89a485158d38f325b62b59be
MykhailoKondrat <mykhailo.kondrat@gmail.com>
2022-07-30 18:39:05 +0200

    chore: add env vars for base api url

--- src/api/apiSlice.ts
+++ src/api/apiSlice.ts
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import {BASE_API_URL} from '../config/env-config';
 
+console.log(BASE_API_URL)
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/v1/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_API_URL }),
   endpoints: () => ({}),
 })
---
+++ src/config/env-config.ts
@@ -0,0 +1,1 @@
+export const BASE_API_URL =  ( !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ) ?  process.env.REACT_APP_DEV_BASE_URL : process.env.REACT_APP_PROD_BASE_URL
