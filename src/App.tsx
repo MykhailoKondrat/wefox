35c90d7c2218a75cd42ddd40e98c7b97b3035bdb
MykhailoKondrat <mykhailo.kondrat@gmail.com>
2022-07-30 12:26:30 +0200

    feat(app): add init setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button, Container} from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+      <Button variant="contained">Hello World</Button>
+    </Container>
+  );
+}
+
+export default App;
